f5ec6b7617d55e39b02276010ab1a21ce163ce9d
stepanovamariia <mariia.alexsa@gmail.com>
2023-11-01 15:34:45 +0200

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframeEl = document.querySelector('iframe');
+
+const player = new Player(iframeEl);
+
+player.on('timeupdate', ({ seconds }) => {
+  updateLocalStorageThrottled(seconds);
+});
+
+const updateLocalStorageThrottled = throttle(seconds => {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}, 1000);
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime) {
+  player.setCurrentTime(parseFloat(savedTime));
+}
